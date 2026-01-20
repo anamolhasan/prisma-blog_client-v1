9c630fd3657c3010cfe0ef184872c875fcc08980
anamolhasan <mdanamolhasan1998@gmail.com>
2026-01-20 20:13:50 +0600

    cookies

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,11 +1,23 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { cookies } from "next/headers";
 
-export default function Home() {
+export default async function Home() {
+  const cookiesStore = await cookies()
+  // console.log(cookiesStore.get("better-auth.session_token"))
+  const res = await fetch('http://localhost:5000/api/auth/get-session', {
+    headers : {
+      Coolie: cookiesStore.toString()
+    },
+    cache:'no-store'
+  })
+
+  const session = await res.json()
+  console.log(session)
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         
-       
+       <Button variant={'outline'}>Click Here</Button>
       </main>
     </div>
   );
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -124,7 +124,7 @@ const Navbar = ({
   ],
   auth = {
     login: { title: "Login", url: "/login" },
-    signup: { title: "Sign up", url: "/register" },
+    signup: { title: "Register", url: "/register" },
   },
   className,
 }: Navbar1Props) => {
